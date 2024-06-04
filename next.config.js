6b066969c932b290b34491891c08c5f501ba2060
blue33199 <blue33199@outlook.com>
2024-06-04 04:43:31 -0400

    Init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
   // output: "export",
-  // distDir: "out",
+  // distDir: "out"
 };
 
 module.exports = nextConfig;
